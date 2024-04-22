607453e2680f7de31d6d4d525b418702d632f20d
Pepijn <128467109+Schaapie09@users.noreply.github.com>
2024-04-22 10:29:24 +0200

    Basic setup vor shapeview

--- Views/ShapeView.js
+++ Views/ShapeView.js
@@ -1,4 +1,24 @@
 class ShapeView {
     constructor() {
+
+    }
+
+    loadTetromino(shape, color) {
+        switch(shape) {
+            case 'straight':
+                switch(color) {
+                    case '#00ff44':
+                        break;
+                }
+                break;
+            case 'square':
+                break;
+            case 'T':
+                break;
+            case 'L':
+                break;
+            case 'skew':
+                break;
+        }
     }
 }
