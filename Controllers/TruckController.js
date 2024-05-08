d8b1ba07bad2a1d9870c65be564536d1c3048d15
lucas <lam.kooijmans@student.avans.nl>
2024-05-08 15:59:48 +0200

    create truck after assembly line is created

--- Controllers/TruckController.js
+++ Controllers/TruckController.js
@@ -11,12 +11,17 @@ class TruckController {
     }
 
     createTruck() {
-        const lengthInput = document.querySelector('#length-input');
-        const widthInput = document.querySelector('#width-input');
-        const intervalInput = document.querySelector('#interval-input');
-        const typeSelect = document.querySelector('#type-select');
-        const truck = new Truck(lengthInput.value, widthInput.value, intervalInput.value, typeSelect.value);
-        this.loadHallManager.getCurrentLoadhall().addTruck(truck);
-        this.truckFormView.renderTruck(truck.length, truck.width);
+        const currentLoadhall = this.loadHallManager.getCurrentLoadhall();
+        if (currentLoadhall.assemblyLines.length > 0) {
+            const lengthInput = document.querySelector('#length-input');
+            const widthInput = document.querySelector('#width-input');
+            const intervalInput = document.querySelector('#interval-input');
+            const typeSelect = document.querySelector('#type-select');
+            const truck = new Truck(lengthInput.value, widthInput.value, intervalInput.value, typeSelect.value);
+            currentLoadhall.addTruck(truck);
+            this.truckFormView.renderTruck(truck.length, truck.width);
+        } else {
+            alert('Please create an assembly line first.');
+        }
     }
 }
