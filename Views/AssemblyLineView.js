40a4ba86ebea545f3116cbabbf5cb00a6511e786
Pepijn <128467109+Schaapie09@users.noreply.github.com>
2024-05-14 21:34:44 +0200

    Drag en drop basics
    Je kan nu een tetrisvorm verplaatsen, voor de rest gebeurt er nog niks. Volgende stap is om het mogelijk te maken om hem daadwerkelijk in de truk te plaatsen, en dat hij in de grid snapped

--- Views/AssemblyLineView.js
+++ Views/AssemblyLineView.js
@@ -1,5 +1,9 @@
 class AssemblyLineView {
     constructor() {
+        this.activePiece = null // Store currently active piece
+        this.startDrag = this.startDrag.bind(this); // Bind the context of `this` to the methods
+        this.dragPiece = this.dragPiece.bind(this);
+        this.endDrag = this.endDrag.bind(this);
     }
 
     renderNewAssemblyLine() {
@@ -8,7 +12,7 @@ class AssemblyLineView {
 
         const shapeContainer = document.createElement('div');
         shapeContainer.classList.add('shapeContainer')
-        shapeContainer.classList.add('grid');
+        shapeContainer.classList.add('assemblyLineGrid');
         assemblyLine.append(shapeContainer);
 
         const truckAssemblyline = document.createElement('div');
@@ -21,6 +25,8 @@ class AssemblyLineView {
 
     renderShape(coords, color, assemblyLine) {
         const shapeContainer = assemblyLine.querySelector('.shapeContainer');
+        const shape = document.createElement('div');
+        shape.classList.add('shape');
         const amount = shapeContainer.children.length;
         for(let i = 0; i < coords.length; i++){
             const coord = coords[i];
@@ -29,8 +35,40 @@ class AssemblyLineView {
             block.style.backgroundColor = color;
             block.style.gridRow = coord[1] + (amount * 1.4);
             block.style.gridColumn = coord[0];
-            shapeContainer.append(block);
-            console.log("Added shape");
+            shape.append(block);
         }
+        shape.addEventListener('mousedown', this.startDrag)
+        document.addEventListener('mouseup', this.endDrag);
+        shapeContainer.append(shape);
     }
+
+    startDrag(event) {
+        event.stopPropagation(); // Prevent event bubbling
+        console.log("StartDrag");
+        this.dragStartX = event.clientX;
+        this.dragStartY = event.clientY;
+
+        document.addEventListener('mousemove', this.dragPiece);
+    }
+
+
+    dragPiece(event) {
+        event.stopPropagation();
+        const tetrisPiece = event.target.parentElement;
+        const newX = event.clientX - this.dragStartX;
+        const newY = event.clientY - this.dragStartY;
+
+        tetrisPiece.style.left = `${newX}px`;
+        tetrisPiece.style.top = `${newY}px`;
+
+        // Prevent text selection during dragging
+        event.preventDefault();
+    }
+
+
+    endDrag() {
+        console.log("endDrag");
+        document.removeEventListener('mousemove', this.dragPiece);
+    }
+
 }
