4ec5c9396374d1d4cd8787463b59dfe08a1ebed5
lucas <lam.kooijmans@student.avans.nl>
2024-05-17 13:27:13 +0200

    add weather and small things

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 window.onload = function() {
+    const apiKey = 'bf56484408341784d4cba93d5d445abb';
+
+    async function getWeather(city) {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+        const weatherData = await response.json();
+        return weatherData;
+    }
+
+    function displayWeather(city) {
+        getWeather(city).then(data => {
+            const weatherInfo = document.querySelector('#weather-info');
+            weatherInfo.innerHTML = `Temperature: ${data.main.temp}°C<br>Weather: ${data.weather[0].main}<br>Wind Speed: ${data.wind.speed} m/s<br><br>`;
+        });
+    }
+
+    const citySelect = document.querySelector('#city-select');
+    citySelect.addEventListener('change', () => {
+        displayWeather(citySelect.value);
+    });
+
     const loadhallView = new LoadHallView();
     const loadhallManager = new LoadHallManager();
     const loadhallController = new LoadHallController(loadhallView, loadhallManager);
